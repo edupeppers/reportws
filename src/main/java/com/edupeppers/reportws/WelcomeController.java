179745d607af5eb1649283e45d2f54aa4ecaa873
muralikrishna_d01 <muralikrishna_d01@10.219.238.80>
2016-01-04 13:37:53 +0530

    - Security dependency is removed - Modified the welcome controller by removing unwanted attributes

--- src/main/java/com/edupeppers/reportws/WelcomeController.java
+++ src/main/java/com/edupeppers/reportws/WelcomeController.java
@@ -2,14 +2,12 @@ package com.edupeppers.reportws;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
-	@RequestMapping(method = { RequestMethod.POST, RequestMethod.GET}, value = { "/welcome" }, headers = "Accept=application/json")
-	@ResponseBody
+	@RequestMapping(method = { RequestMethod.POST, RequestMethod.GET}, value = { "/welcome" }, produces="application/json")
 	public String welcome() {
 		return "Welcome to ReportWs Framework... :)";
 	}
